b3ca41af76f064f7b9155104d9c20201bcb02b58
Jonathan Tan <jhow.tan@gmail.com>
2015-04-12 16:46:15 +0800

    Fix category navigation

--- app/scripts/my-app.js
+++ app/scripts/my-app.js
@@ -27,19 +27,13 @@ myApp.onPageInit('dir', function (page) {
                             '</li>');
     }
     for (var i=0; i<categories.length-1; i++) {
-      $('#cat-list').append('<li>' +
-                                '<a href="category.html" class="item-link item-content" data-id="'+ i +'">' +
+      $('#cat-list').append('<li class="cat-page" data-id="'+i +'">' +
+                                '<a href="category.html" class="item-link item-content">' +
                                 '<div class="item-inner">' +
                                     '<div class="item-title">' + categories[i].name  + '</div>'+
                                 '</div></a>'+
                             '</li>');
     }
-
-    // var indexPass;
-    // $('.cat-page').on('click', function() {
-    //     indexPass = $('this').data('id');
-    //     console.log(indexPass);
-    // })
 
     var mySearchbar = myApp.searchbar('.searchbar', {
         searchList: '.list-block-search',
@@ -51,29 +45,62 @@ myApp.onPageInit('dir', function (page) {
         createStorePage(index);
     });
 
-
+    $$('.cat-page').on('click', function(){
+        var index = $(this).data('id');
+        createCatPage(index);
+    });
 });
 
-myApp.onPageInit('category', function (page) {
-    console.log($this.data('id'));
-    var index = $(this).data('id');
-    var catShopList = [];
-    for (var i=0; i<categories[index].shops.shop.length; i++) {
-        catShopList.push(categories[index].shops.shop[i]);
-    }
-    for (var i=0; i< catShopList.length; i++) {
-        $('#shops-list').append('<li class="item-content shop-page" data-id="'+i +'"">' +
-                                '<div class="item-inner">' +
-                                    '<div class="item-title">' + shops[i].name  + '</div>'+
-                                '</div>'+
-                                '</li>');
-        }
-    }
-    // run createContentPage func after link was clicked
-    // $$('.shop-page').on('click', function () {
-    //     createContentPage();
-    // });
-);
+function createCatPage(index) {
+  var catShopList = [];
+  for (var i=0; i<categories[index].shops.shop.length; i++) {
+      catShopList.push(categories[index].shops.shop[i]);
+  }
+  mainView.router.loadContent(
+    '<!-- Top Navbar-->' +
+    '<div class="navbar">' +
+    '  <div class="navbar-inner">' +
+    '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+    '    <div class="center sliding">' + categories[index].name + '</div>' +
+    '  </div>' +
+    '</div>' +
+    '<div class="pages navbar-through toolbar-through">' +
+    '  <div data-page="category" class="page">' +
+    '    <div class="page-content">' +
+    '      <div class="content-block-title">' + categories[index].name + '</div>' +
+    '        <ul id="shops-list"></ul>' +
+    '      </div>' +
+    '   </div>' +
+    '</div>');
+  for (var i=0; i< catShopList.length; i++) {
+      $('#shops-list').append('<li class="item-content shop-page" data-id="'+i+'"">' +
+                              '<div class="item-inner">' +
+                                  '<div class="item-title">' + catShopList[i].name  + '</div>'+
+                              '</div>'+
+                              '</li>');
+      }
+}
+
+// myApp.onPageInit('category', function (page) {
+//     console.log($this.data('id'));
+//     var index = $(this).data('id');
+//     var catShopList = [];
+//     for (var i=0; i<categories[index].shops.shop.length; i++) {
+//         catShopList.push(categories[index].shops.shop[i]);
+//     }
+//     for (var i=0; i< catShopList.length; i++) {
+//         $('#shops-list').append('<li class="item-content shop-page" data-id="'+i +'"">' +
+//                                 '<div class="item-inner">' +
+//                                     '<div class="item-title">' + shops[i].name  + '</div>'+
+//                                 '</div>'+
+//                                 '</li>');
+//         }
+//     }
+//     // run createContentPage func after link was clicked
+//     // $$('.shop-page').on('click', function () {
+//     //     createContentPage();
+//     // });
+// );
 
 
 myApp.onPageInit('navi', function (page) {
@@ -113,8 +140,7 @@ function createStorePage(index) {
         '      </div>' +
         '    </div>' +
         '  </div>' +
-        '</div>' +
-        ''
+        '</div>'
     );
 	return;
 }
